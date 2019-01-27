565ee6a531069df24a11d6b8a6b38b5eeeec24d0
Aniol Marti <aniol@aniolmarti.cat>
2019-01-27 23:34:55 +0100

    M'havia oblidat de treure la barra de l'html

--- index.php
+++ index.php
@@ -63,9 +63,6 @@ include "includes/conf-tvshows.php";
             <input type="submit" name="submit" value="Enviar" >
             <input type="reset" value="Reiniciar" onClick="amaga();">
         </div>
-        <progress id="progressBar" value="0" max="100" style="width:300px;"></progress>
-        <h3 id="status"></h3>
-        <p id="loaded_n_total"></p>
     </form>
 </div>
 </body>
