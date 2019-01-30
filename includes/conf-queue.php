8aa69593205dc935cb4d2324789b78afdaf196ff
Aniol Marti <aniol@aniolmarti.cat>
2019-01-30 17:54:41 +0100

    Change new line in queue

--- includes/conf-queue.php
+++ includes/conf-queue.php
@@ -2,6 +2,6 @@
 
 function add_queue($reldir) {
     $queue_file = "queue.php";
-    $line = "\n<h4><a href=https://multimedia.xarxacatala.cat/".$reldir.">".$reldir."</a></h4>";
+    $line = "<h4><a href=https://multimedia.xarxacatala.cat/".$reldir.">".$reldir."</a></h4>\n";
     file_put_contents($queue_file, $line, FILE_APPEND | LOCK_EX);
 }
