9e00ad0e1d5e6a78b3e6009df4cebbc53bad419b
Aniol Marti <aniol@aniolmarti.cat>
2019-08-16 01:08:01 +0200

    Using a parameter for the subs reduces the amount of code

--- includes/encode.php
+++ includes/encode.php
@@ -4,15 +4,8 @@ include "conf-queue.php";
 include_once "logging.php";
 
 function encode($subs, $reldir, $base) {
-    if ($subs) {
-        logs("started encoding with subtitles ".$base.$reldir.".\n");
-        exec("perl scripts/subs_encode.pl \"".$base."\" \"".$reldir."\" > /dev/null 2>&1 &");
-        add_queue($reldir);
-    }
-    else {
-        logs("started encoding ".$base.$reldir.".\n");
-        exec("perl scripts/encode.pl \"".$base."\" \"".$reldir."\" > /dev/null 2>&1 &");
-        add_queue($reldir);
-    }
+    logs("started encoding ".$base.$reldir.".\n");
+    exec("perl scripts/encode.pl \"".$base."\" \"".$reldir."\" ".$subs." > /dev/null 2>&1 &");
+    add_queue($reldir);
 }
 ?>
--- scripts/encode.pl
+++ scripts/encode.pl
@@ -4,17 +4,24 @@ use warnings;
 use autodie qw(:all);
 
 sub encode {
-    my $queue_file = "queue.php";
-    
+    my $queue_file          = "queue.php";
+    my $subs                = "$_[2]";
+
     # Path related variables.
-    my $base = "$_[0]";
-    my $relpath = "$_[1]";
-    my $abspath = "$base$relpath";
+    my $base                = "$_[0]";
+    my $relpath             = "$_[1]";
+    my $abspath             = "$base$relpath";
     my $abspath_noextension = substr($abspath, 0, rindex($abspath, '.') - length($abspath));
-    my $escaped_relpath = quotemeta($relpath);
+    my $escaped_relpath     = quotemeta($relpath);
+    my $escaped_abspath     = quotemeta($abspath);
     
     # Encode file
-    if (qx{mediainfo $abspath | grep \"Writing library\" | sed -n 2p | cut -d \":\" -f 2 | cut -d \" \" -f 2} ne "x264\n") {
+    if ($subs eq "with-subs") {
+        my @ffmpeg = ("ffmpeg", "-nostats", "-nostdin", "-i", "$abspath", "-map", "0:v:0", "-map", "0:a:0", "-vf",
+                        "subtitles=$escaped_abspath", "$abspath_noextension.mp4");
+        system(@ffmpeg);
+    }
+    elsif (qx{mediainfo $abspath | grep \"Writing library\" | sed -n 2p | cut -d \":\" -f 2 | cut -d \" \" -f 2} ne "x264\n") {
         my @ffmpeg = ("ffmpeg", "-nostats", "-nostdin", "-i", "$abspath", "-vcodec", "libx264", "-acodec", "libmp3lame",
                         "-map", "0:v:0", "-map", "0:a:0", "$abspath_noextension.mp4");
         system(@ffmpeg);
@@ -37,4 +44,4 @@ sub encode {
     return;
 }
 
-encode($ARGV[0], $ARGV[1]);
+encode($ARGV[0], $ARGV[1], $ARGV[2]);
